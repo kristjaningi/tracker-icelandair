1725074e534bb6285b84cab591e6790a8ffbedb9
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2021-03-20 21:49:08 +0000

    Fetch data and display flights on the map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Head from 'next/head';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { MapContext } from 'react-map-gl';
 
-export default function Home() {
+function CustomMarker(props) {
+  const context = useContext(MapContext);
+
+  const { longitude, latitude, flight } = props;
+
+  console.log(flight);
+
+  const [x, y] = context.viewport?.project([longitude, latitude]);
+
+  return (
+    <div className="flex flex-col justify-center" style={{ position: 'absolute', left: x, top: y }}>
+      <p className="text-red-400">{flight.ident}</p>
+    </div>
+  );
+}
+
+function Home({ data }: any) {
   const [viewport, setViewport] = useState({
     latitude: 63.0,
     longitude: -20.982,
@@ -43,7 +59,43 @@ export default function Home() {
           }>
         ) => setViewport(viewport)}
         mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
-      />
+      >
+        {data.map((flight) => {
+          // console.log(flight);
+
+          return (
+            <CustomMarker
+              key={flight.faFlightID}
+              longitude={flight.longitude}
+              latitude={flight.latitude}
+              flight={flight}
+            />
+          );
+        })}
+      </ReactMapGL>
     </div>
   );
 }
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    // Get all active in air Icelandair flights
+    `https://flightxml.flightaware.com/json/FlightXML2/SearchBirdseyeInFlight?query={match ident ICE*} {true inAir}`,
+    {
+      headers: {
+        authorization: `Basic ${process.env.FLIGHT_AWARE_API_KEY}`,
+      },
+    }
+  );
+  const data = await res.json();
+
+  if (!data) {
+    console.log('NO DATA');
+  }
+
+  return {
+    props: { data: data.SearchBirdseyeInFlightResult.aircraft },
+  };
+}
+
+export default Home;
