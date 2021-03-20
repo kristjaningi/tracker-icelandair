ecc0199501347a3b35079bff86831f50f16b0150
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2021-03-20 23:53:22 +0000

    Sidebar update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { IFlightData } from '../types';
+import { formatAirportCode } from '../helpers';
 
 interface Props {
   flights: IFlightData[];
@@ -14,14 +15,35 @@ export default function Sidebar({ flights }: Props) {
       <div className="flex-grow">
         {flights ? (
           flights.map((flight: IFlightData) => (
-            <div key={flight.faFlightID} className="p-4 border-b-2">
+            <div key={flight.faFlightID} className="p-3 border-b-2">
               <div className="flex">
-                <div className="font-semibold text-lg">{flight.ident}</div>
+                <img className="" src="/logo-small.png" width="35" />
+                <div className="font-semibold text-lg ml-3">{flight.ident}</div>
               </div>
-              <div className="flex flex-row justify-between items-center mt-2">
-                <div className="">
+              <div className="flex flex-row justify-between items-center mt-3">
+                <div className="border-2">
                   <div className="text-xs text-gray-500">Origin</div>
-                  <div className="">{flight.origin}</div>
+                  <div className="">{formatAirportCode(flight.origin)}</div>
+                </div>
+
+                <svg
+                  className=""
+                  style={{ transformOrigin: 'center center 0px', transform: 'rotate(90deg)' }}
+                  fill="#003a7d"
+                  stroke="#003a7d"
+                  strokeWidth="0"
+                  height="30"
+                  width="30"
+                  aria-hidden="true"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M10.18 9"></path>
+                  <path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"></path>
+                </svg>
+
+                <div className="border-2">
+                  <div className="text-xs text-gray-500">Destination</div>
+                  <div className="">{formatAirportCode(flight.destination)}</div>
                 </div>
               </div>
             </div>
---
+++ helpers.ts
@@ -0,0 +1,8 @@
+export function formatAirportCode(airport: string) {
+  if (airport.length > 4 || airport.length < 4 || airport.length == 0) {
+    // console.log(airport, ' string is to long);
+    return 'Unkown';
+  }
+
+  return airport;
+}
